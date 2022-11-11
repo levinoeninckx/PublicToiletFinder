3fb86077aacda6cf8af217cf2232de0673831be0
levi noeninckx <levi.noeninckx@gmail.com>
2022-11-11 15:16:07 +0100

    Add json parse method

--- app/src/main/java/edu/ap/publictoiletfinder/MainActivity.kt
+++ app/src/main/java/edu/ap/publictoiletfinder/MainActivity.kt
@@ -2,10 +2,15 @@ package edu.ap.publictoiletfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.beust.klaxon.Parser
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun parse(name: String): Any? {
+        val cls = Parser::class.java
+        return cls.getResourceAsStream(name)?.let { inputStream -> return Parser.default().parse(inputStream) }
     }
 }
