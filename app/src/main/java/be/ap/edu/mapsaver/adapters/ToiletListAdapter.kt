8d1b5b749f184367a7c8a695b7a54226a858893c
levi noeninckx <levi.noeninckx@gmail.com>
2022-12-10 15:03:00 +0100

    add listview activity

--- app/src/main/java/be/ap/edu/mapsaver/adapters/ToiletListAdapter.kt
+++ app/src/main/java/be/ap/edu/mapsaver/adapters/ToiletListAdapter.kt
@@ -12,7 +12,7 @@ import kotlinx.android.synthetic.main.toilet_item.view.*
 class ToiletListAdapter(var dataSet: List<Attributes>): RecyclerView.Adapter<ToiletListAdapter.ToiletListViewHolder>(){
     class ToiletListViewHolder(val view: View): RecyclerView.ViewHolder(view), View.OnClickListener{
 
-        val toiletId: TextView = view.findViewById(R.id.toilet_id)
+        val toiletId: TextView = view.findViewById(R.id.toilet_gender)
         val toiletStreet: TextView = view.findViewById(R.id.toilet_street)
 
         override fun onClick(p0: View?) {
@@ -33,8 +33,14 @@ class ToiletListAdapter(var dataSet: List<Attributes>): RecyclerView.Adapter<Toi
     }
 
     override fun onBindViewHolder(holder: ToiletListViewHolder, position: Int) {
-        holder.view.toilet_id.text = dataSet[position].id.toString()
-        holder.view.toilet_street.text = dataSet[position].straat
+        if(dataSet[position].doelgroep == null){
+            holder.view.toilet_gender.text = "Genderneutraal"
+        } else {
+            holder.view.toilet_gender.text = dataSet[position].doelgroep
+        }
+        if(dataSet[position].straat != null){
+            holder.view.toilet_street.text = dataSet[position].straat
+        }
     }
 
     override fun getItemCount() = dataSet.count()
